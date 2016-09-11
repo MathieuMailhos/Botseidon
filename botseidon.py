3a141086db486e339793f71f6c531a943ef3c8ee
Mathieu Mailhos <mathieumailhos@gmail.com>
2016-09-11 03:18:15 -0700

    Improved python shell

--- botseidon.py
+++ botseidon.py
@@ -1,7 +1,28 @@
 #! /usr/bin/env python
 
 import json
+import requests
 from watson_developer_cloud import ConversationV1
+
+
+# City with its name, wind strength and weather
+class City:
+    def __init__(self, name):
+        self.name = name
+
+    def _get_city_data(self):
+        r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + self.name + '&appid=' + conf['openweather_key'])
+        body = json.loads(r.text)
+        print(body)
+        if 'weather' in body and len(body['weather']) > 0 and 'main' in body['weather']:
+            self.weather = body['weather']['main']
+        else:
+            self.weather = None
+        if 'wind' in body and 'speed' in body['wind']:
+            self.wind = float(body['wind']['speed']) * 1.94384
+        else:
+            self.wind = None
+
 
 # Read and parse the configuration file
 def read_conf(name="config.json"):
@@ -10,9 +31,9 @@ def read_conf(name="config.json"):
         config_data = json.load(config_file)
     return config_data
 
-#Ask Botseidon
-def ask(input_text):
-    return conversation.message(workspace_id= conf['workspace_id'], message_input= {"text": input_text})
+# Ask Botseidon
+def ask(input_text, current_context):
+    return conversation.message(workspace_id= conf['workspace_id'], message_input= {"text": input_text}, context= current_context)
 
 # Set up the environment
 conf = read_conf()
@@ -22,11 +43,21 @@ if conf is None:
 
 conversation = ConversationV1(username=conf['username'], password=conf['password'], version=conf['version'])
 
-#Permanent shell
+city = None
+context = {}
+# Permanent shell
 while True:
-    text = input(">>>")
-    res = ask(text)
+    text = input(">>> ")
+    res = ask(text, context)
+    context = conversation_id =res["context"]
     if "output" in res and "text" in res["output"] and len(res["output"]["text"]) > 0:
+        # Intercepting the place and filling the city wind and weather values
+        if "You live in:" in res["output"]["text"][0]:
+            city = City(res["output"]["text"][0].split(':')[-1])
+            city._get_city_data()
+            print("asking for the wind")
+            res = ask("The wind is blowing " + str(city.wind), context )
+
         print(res["output"]["text"][0])
     else:
         print("> Error: could not understand the meaning")
